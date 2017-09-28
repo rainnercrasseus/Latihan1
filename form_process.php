29c76a660d5b1ebb720aa280b14c49ec2bb0e92a
rainnercrasseus <32196271+rainnercrasseus@users.noreply.github.com>
2017-09-28 16:23:11 +0700

    LEVEL 1
    LEVEL 1 - External Page Submission

---
+++ form_process.php
@@ -0,0 +1,27 @@
+<?php
+if(isset($_POST['submit'])){
+	
+	$name = array("rainner", "maria", "johndoe", "nathan", "janedoe");
+	
+	$minimum = 5;
+	$maximum = 10;
+	
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	
+	if(strlen($username) < $minimum){
+		echo "Username harus memiliki panjang 5 atau lebih";
+	}
+	
+	if(strlen($username) > $maximum){
+		echo "Username tidak boleh lebih panjang dari 10";
+	}
+	
+	if(!in_array($username, $name)){
+		echo "Maaf, akses ditolak";
+	}else{
+		echo "Selamat datang";
+	}
+	
+}
+?>
