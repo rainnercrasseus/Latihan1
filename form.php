c67a14a4ad6fa5dd3bf520d472e11c18a4f7ec30
rainnercrasseus <32196271+rainnercrasseus@users.noreply.github.com>
2017-09-28 16:06:59 +0700

    LEVEL 1
    LEVEL 1 - Validating the Form Values

--- form.php
+++ form.php
@@ -1,11 +1,28 @@
 <?php
-if(isset($_POST['submit'])){ 
+if(isset($_POST['submit'])){
+	
+	$name = array("rainner", "maria", "johndoe", "nathan", "janedoe");
+	
+	$min = 5;
+	$max = 10;
+	
 	$username = $_POST['username'];
 	$password = $_POST['password'];
 	
-	echo $username;
-	echo "<br/>";
-	echo $password;
+	if(strlen($username) < $minimum){
+		echo "Username harus memiliki panjang 5 atau lebih";
+	}
+	
+	if(strlen($username) > $maximum){
+		echo "Username tidak boleh lebih panjang dari 10";
+	}
+	
+	if(!in_array($username, $name)){
+		echo "maaf, akses ditolak";
+	}else{
+		echo "selamat datang";
+	}
+	
 }
 ?>
 
