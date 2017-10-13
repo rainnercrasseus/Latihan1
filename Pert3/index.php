c72b1842d5169b0a60ce93862a958949897dbbf4
rainnercrasseus <32196271+rainnercrasseus@users.noreply.github.com>
2017-10-13 12:16:51 +0700

    Index

---
+++ Pert3/index.php
@@ -0,0 +1,85 @@
+<?php // filename: index.php
+	//koneksi
+	include("koneksi.php");
+	
+	//query
+	$query = "SELECT * FROM kontak";
+	$hasil = mysqli_query($db, $query);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Data Mahasiswa Kalbis</title>
+	<link rel="stylesheet" href="
+	<style>
+		#tbody{background:#000000;}
+		#menu{background:#CCCCFF;}
+		#konten{background:#CCCC33;}
+		#filter{background:#33FFCC;}
+		#search{background:#33FFCC;}
+		#footer{background:cyan;}
+	</style>
+</head>
+<body>
+<h1>Phone Book</h1>
+<div id="menu">
+	<ul>
+		<li><a href="index.php">Kontak</a></li>
+		<li><a href="kategori.php">Kategori</a></li>
+	</ul>
+</div>
+<div id="filter">
+	<b>Filter berdasarkan kategori: </b>
+	<form action="" method="post">
+		<select name="">
+			<option value=""></option>
+		</select>
+		<input type="submit" name="filter" value="Filter" />
+	</form>
+</div>
+<div id="search">
+	<b>Search: </b>
+	<form action="search.php" method="post">
+		<input type="text" name="search_text" />
+		<input type="submit" name="cari" value="Cari" />
+	</form>
+</div>
+<div id="konten">
+	<h2>Kontak</h2>
+	<a href="form_tambah_kontak.php">Tambah Kontak</a>
+	<table border="1">
+		<thead>
+			<tr>
+				<th>No.</th>
+				<th>Nama</th>
+				<th>Hp.</th>
+				<th>Email</th>
+				<th>Kategori</th>
+				<th>Action</th>
+			</tr>
+		</thead>
+		<tbody>
+		<?php
+		while($row = mysqli_fetch_assoc($hasil)){
+			?>
+			<tr>
+				<td><?php echo $row['id']; ?></td>
+				<td><?php echo $row['nama']; ?></td>
+				<td><?php echo $row['phone']; ?></td>
+				<td><?php echo $row['email']; ?></td>
+				<td><?php echo $row['kategori_id']; ?></td>
+				<td>							
+					<a href="form_edit_kontak.php?id=<?php echo $row['id'];?>">Edit</a> | 
+					<a href="delete_kontak.php?id=<?php echo $row['id'];?>">Delete</a>
+				</td>
+			
+			</tr>
+			<?php
+			}
+			?>
+		</tbody>
+	</table>
+</div>
+</body>
+</html>
