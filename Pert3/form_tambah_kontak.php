5cf70d3c44b9effdf3457d2b93a0cf0a59d8fca6
rainnercrasseus <32196271+rainnercrasseus@users.noreply.github.com>
2017-10-13 12:16:09 +0700

    Form Tambah Kategori

---
+++ Pert3/form_tambah_kontak.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Data Mahasiswa Kalbis</title>
+	<link rel="stylesheet" href=
+	<style>
+		#tbody{background:#000000;}
+		#menu{background:#CCCCFF;}
+		#konten{background:#CCCC33;}
+		#filter{background:#33FFCC;}
+		#search{background:#33FFCC;}
+		#footer{background:cyan;}
+	</style>
+</head>
+<body>
+<h1>Phone Book</h1>
+<div id="menu">
+	<ul>
+		<li><a href="index.php">Kontak</a></li>
+		<li><a href="kategori.php">Kategori</a></li>
+	</ul>
+</div>
+<div id="konten">
+	<h2>Tambah Kontak</h2>
+	<form action="proses_tambah_kontak.php" method="post">
+	<tbody>
+		Nama:
+		<input type="text" name="nama" />
+		<br />
+		Phone:
+		<input type="text" name="phone" />
+		<br />
+		Email:
+		<input type="text" name="email" />
+		<br />
+		Kategori:
+		<select name="kategori"/> 
+		<?php
+		include("koneksi.php");
+		$db = mysqli_connect("localhost","root", "", "kalbis_web");
+		$query = "select * from kategori";
+		$hasil = mysqli_query($db,$query);
+		while($data=mysqli_fetch_array($hasil)){
+		echo "<option value=$data[id]>$data[keterangan]</option>";
+		
+		}
+		?>
+		<br />
+		</select>	
+		<input type="submit" name="submit" value="Simpan" />
+		
+	</form>
+	</tbody>
+</div>
+</body>
+</html>
