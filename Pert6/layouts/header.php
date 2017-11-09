17fbbb76fa19101c3c195a12cca11acb9f6a7450
rainnercrasseus <32196271+rainnercrasseus@users.noreply.github.com>
2017-11-10 03:09:51 +0700

    LAYOUTS

---
+++ Pert6/layouts/header.php
@@ -0,0 +1,10 @@
+<html lang="en">
+	<head>
+	<title></title>
+	<link href="stylesheets/public.css" media="all" rel="stylesheet" type="text/css" />
+	</head>
+<body>
+
+ <div id="header">
+	<h1>Widget Corp</h1>
+	</div>
